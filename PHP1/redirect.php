dc02f99a0c139045b83b8ce74a4162190688a220
yanqr <giancant3@gmail.com>
2026-03-10 16:01:03 +0800

    Update redirect.php

--- PHP1/redirect.php
+++ PHP1/redirect.php
@@ -26,6 +26,27 @@
     <h2>Data is sent here, and it is store at <?php echo $req_type; ?> variable</h2>
     <table>
         <tr>
+            <td width="120">First Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['fname'] : $_POST['fname']; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Middle Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['mname'] : $_POST['mname']; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>Last Name:</td>
+            <td style="text-decoration: underline">
+                <!-- Use ternary operator to check if the request type is GET or POST -->
+                <?php echo ($req_type == '$_GET') ? $_GET['lname'] : $_POST['lname']; ?>
+            </td>
+        </tr>
+        <tr>
             <td width="120">Age:</td>
             <td style="text-decoration: underline">
                 <!-- Use ternary operator to check if the request type is GET or POST -->
